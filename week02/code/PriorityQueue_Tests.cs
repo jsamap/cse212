2d6df98bf71415ef4aa269ee7e2d5cced81640f4
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-05-23 16:09:30 -0600

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,56 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Dequeue from empty queue.
+    // Expected Result: InvalidOperationException with message "The queue is empty.".
+    // Defect(s) Found: None, just checking that the queue is not empty.
+    public void TestPriorityQueue_EmptyQueue()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var ex = Assert.ThrowsException<InvalidOperationException>(() => priorityQueue.Dequeue());
+        Assert.AreEqual("The queue is empty.", ex.Message);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Single person enqueue/dequeue.
+    // Expected Result: Person is returned and removed.
+    // Defect(s) Found: Code does not remove the item. Need to add _queue.RemoveAt() to remove the person.
+    public void TestPriorityQueue_SinglePerson()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Bob", 1);
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Bob", result);
+        Assert.AreEqual("[]", priorityQueue.ToString());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Multiple items, highest priority removed.
+    // Expected Result: Person with highest priority returned.
+    // Defect(s) Found: Original code skipped last element because of the range. Need to remove the -1 from the for loop in the dequeue function.
+    public void TestPriorityQueue_HighestPriority()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("Bob", 1);
+        priorityQueue.Enqueue("Tim", 3);
+        priorityQueue.Enqueue("Sue", 2);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Tim", result);
+    }
+
+    [TestMethod]
+    // Scenario: Multiple items with same highest priority.
+    // Expected Result: First one (FIFO) with same high priority removed.
+    // Defect(s) Found: Code removes the last item due to a wrong validation ">=". It should be ">".
+    public void TestPriorityQueue_FifoPriority()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("Bob", 5);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Sue", 5);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("Bob", result);
+    }
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: The first element is not matching the expected result because the Enqueue function is inserting the value to front of the queue instead of the back. The solution is to replace the 0 for Length in "_queue.Insert(0, person);". 
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +43,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: The order of the queue is incorrect because the Enqueue function is inserting the value to front of the queue instead of the back. The solution is to replace the 0 for Length in "_queue.Insert(0, person);".
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +85,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with infinite turns (in this case, Tim) are not being requeued. There should be an additional validation inside GetNextPerson() that requeues the person if the turn is less than 1.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +116,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with infinite turns (in this case, Tim) are not being requeued. There should be an additional validation inside GetNextPerson() that requeues the person if the turn is less than 1. Additionally, the turn number should not be decreased.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +143,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: None. This function worked fine from the start.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
